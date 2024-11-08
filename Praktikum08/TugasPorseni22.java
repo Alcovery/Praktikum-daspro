679174c46cdc47bd66ea27023e034fcd93c27862
unknown <Ubaidillahulil@gmail.com>
2024-11-08 09:42:58 +0700

    Praktikum10

---
+++ Praktikum08/TugasPorseni22.java
@@ -0,0 +1,31 @@
+package Praktikum08;
+
+import java.util.Scanner;
+
+public class TugasPorseni22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        while(i <=4) {
+            System.out.println("Masukkkan cabang olahraga : ");
+            String cabor = sc.nextLine();
+            String atlet = "";
+        
+        for (int j = 1 ; j <= 5; j++ ){
+            System.out.println("Masukkan nama atlet " + cabor + " ke " + j + " :");
+            String nama = sc.nextLine();
+            atlet += nama;
+
+            if ( j < 5){
+                atlet += ",";
+            }
+        }    
+        System.out.print("Pemain cabang olahraga " + cabor + " adalah : " + atlet);
+        i++;
+        System.out.println();
+        System.out.println();
+        }
+        sc.close();
+    }
+}
